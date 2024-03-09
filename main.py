ff53ea2e52b44f8cca10c3bd842c99f5b9719521
Chukhrai Artur <chukhraiartur@gmail.com>
2024-03-09 16:07:27 +0200

    feat(QTSP-1): added pagination to the parser

--- main.py
+++ main.py
@@ -17,34 +17,44 @@ def read_from_html(filename) -> str:
     return html
 
 
-def parse(response: Response) -> list[dict]:
-    selector: Selector = Selector(text=response.text)
+def parse(response: Response, headers: dict) -> list[dict]:
     data: list[dict] = []
 
-    quotes: SelectorList = selector.css('.quote')
-    for quote in quotes:
-        text: str = quote.css('.text::text').get().strip()[1:-1]
-        author: str = quote.css('[itemprop="author"]::text').get().strip()
-        link: str = response.url + quote.css('span a::attr(href)').get()[1:]
-        tags: list[dict] = [
-            {
-                'name': tag.css('::text').get().strip(),
-                'link': response.url + tag.css('::attr(href)').get()[1:]
-            }
-            for tag in quote.css('.tags .tag')
-        ]
+    while True:
+        selector: Selector = Selector(text=response.text)
+        quotes: SelectorList = selector.css('.quote')
+        for quote in quotes:
+            text: str = quote.css('.text::text').get().strip()[1:-1]
+            author: str = quote.css('[itemprop="author"]::text').get().strip()
+            link: str = response.url + quote.css('span a::attr(href)').get()[1:]
+            tags: list[dict] = [
+                {
+                    'name': tag.css('::text').get().strip(),
+                    'link': response.url + tag.css('::attr(href)').get()[1:]
+                }
+                for tag in quote.css('.tags .tag')
+            ]
 
-        data.append({
-            'text': text,
-            'author': author,
-            'link': link,
-            'tags': tags
-        })
+            data.append({
+                'text': text,
+                'author': author,
+                'link': link,
+                'tags': tags
+            })
+
+        next_button: str = selector.css('.pager .next a::attr(href)').get()
+        if next_button:
+            url = 'https://quotes.toscrape.com' + next_button
+            print(url)
+            response: Response = requests.get(url=url, headers=headers)
+        else:
+            break
+
 
     return data
 
 
-headers: dict[str] = {
+headers: dict = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
 }
 
@@ -53,5 +63,6 @@ url: str = 'https://quotes.toscrape.com'
 response: Response = requests.get(url=url, headers=headers)
 save_to_html(response=response, filename='quotes')
 
-quotes_to_scrape_data: list[dict] = parse(response)
+quotes_to_scrape_data: list[dict] = parse(response, headers)
 print(json.dumps(quotes_to_scrape_data, indent=2, ensure_ascii=False))
+print(len(quotes_to_scrape_data))
